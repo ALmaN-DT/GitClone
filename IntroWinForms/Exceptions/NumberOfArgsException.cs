692a4c1f3d478c101c50d74c187961f6a86a2aa0
ALmaN-DT <bansfoll2000@gmail.com>
2018-07-02 20:46:19 +0600

    Добивл проект

---
+++ IntroWinForms/Exceptions/NumberOfArgsException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroWinForms.Exceptions
+{
+    [Serializable]
+    class NumberOfArgsException : System.Exception, ISerializable
+    {
+        public int NumberOfArgs { get; set; }
+
+        public NumberOfArgsException(int numberOfArgs)
+        {
+            NumberOfArgs = numberOfArgs;
+            this.HelpLink = "http://google.com";
+        }
+
+        public NumberOfArgsException(int numberOfArgs, string message) : base(message)
+        {
+            NumberOfArgs = numberOfArgs;
+            this.HelpLink = "http://google.com";
+        }
+
+
+        public NumberOfArgsException(int numberOfArgs, string message, Exception exception) : base(message, exception)
+        {
+            NumberOfArgs = numberOfArgs;
+            this.HelpLink = "http://google.com";
+        }
+
+        protected NumberOfArgsException(SerializationInfo info, StreamingContext context)
+        {
+            NumberOfArgs = (int)info.GetValue("NumberOfArgs", typeof(int));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("NumberOfArgs", NumberOfArgs, typeof(int));
+        }
+
+    }
+}
